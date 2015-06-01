4cf97088737049ffdff02aa45286de0f0492ebd9
Manuel Navarro <mn.slir@gmail.com>
2015-06-01 08:55:27 -0700

    Updated inline documentation

--- onlineRegistration_app/App_Data/BusinessLayer/SeriesInfo.cs
+++ onlineRegistration_app/App_Data/BusinessLayer/SeriesInfo.cs
@@ -5,75 +5,100 @@ using System.Web;
 
 namespace onlineRegistration_app.BusinessLayer
 {
-   public class SeriesInfo
-   {
-      public static string Name { get { return "Series"; } }
+    /// <summary>
+    /// Handles the XML Data of Schedule Series (by month).
+    /// </summary>
+    public class SeriesInfo
+    {
+        public static string Name { get { return "Series"; } }
 
-      private string title;
-      private bool customTitle;
-      private DateTime date;
-      private bool visible;
-      private string xpathaddress;
+        private string title;
+        private bool customTitle;
+        private DateTime date;
+        private bool visible;
+        private string xpathaddress;
 
-      public SeriesInfo()
-      {
-         title = xpathaddress = "";
-         date = DateTime.MinValue;
-         visible = true;
-      }
+        /// <summary>
+        /// Class Initializer
+        /// </summary>
+        public SeriesInfo()
+        {
+            title = xpathaddress = "";
+            date = DateTime.MinValue;
+            visible = true;
+        }
 
-      public DateTime Date
-      {
-         get { return this.date; }
-         set { this.date = value; }
-      }
+        /// <summary>
+        /// Full date for the Series.
+        /// </summary>
+        public DateTime Date
+        {
+            get { return this.date; }
+            set { this.date = value; }
+        }
 
-      public int Month
-      { get { return this.date.Month; } }
+        /// <summary>
+        /// Month part for the Series.
+        /// </summary>
+        public int Month
+        { get { return this.date.Month; } }
 
-      public string MonthName
-      { get { return this.date.ToString("MMMM"); } }
+        /// <summary>
+        /// Month name for the Series (January, February, etc).
+        /// </summary>
+        public string MonthName
+        { get { return this.date.ToString("MMMM"); } }
 
-      public string MonthAndYear
-      { get { return MonthName + " " + Year.ToString(); } }
+        /// <summary>
+        /// Month and Year parts combined as a string for the Series.
+        /// </summary>
+        public string MonthAndYear
+        { get { return MonthName + " " + Year.ToString(); } }
 
-      public string DisplayTitle
-      {
-         get
-         {
-            string rTitle = Title;
+        /// <summary>
+        /// Combines Title and MonthAndYear together, or returns just Title if CustomTitle is set.
+        /// </summary>
+        public string DisplayTitle
+        {
+            get
+            {
+                string rTitle = Title;
 
-            if (!CustomTitle) rTitle += " " + MonthAndYear;
+                if (!CustomTitle) rTitle += " " + MonthAndYear;
 
-            return rTitle;
-         }
-      }
+                return rTitle;
+            }
+        }
 
-      public bool CustomTitle
-      {
-         get { return this.customTitle; }
-         set { this.customTitle = value; }
-      }
+        /// <summary>
+        /// When set to True, the Series DisplayTitle will only return what is set for Title,
+        /// otherwise, it will return Title and MonthAndYear combined.
+        /// </summary>
+        public bool CustomTitle
+        {
+            get { return this.customTitle; }
+            set { this.customTitle = value; }
+        }
 
-      public string Title
-      {
-         get { return this.title; }
-         set { this.title = value; }
-      }
+        public string Title
+        {
+            get { return this.title; }
+            set { this.title = value; }
+        }
 
-      public bool Visible
-      {
-         get { return this.visible; }
-         set { this.visible = value; }
-      }
+        public bool Visible
+        {
+            get { return this.visible; }
+            set { this.visible = value; }
+        }
 
-      public int Year
-      { get { return this.date.Year; } }
+        public int Year
+        { get { return this.date.Year; } }
 
-      public string XPathAddress
-      {
-         get { return this.xpathaddress; }
-         set { this.xpathaddress = value; }
-      }
-   }
+        public string XPathAddress
+        {
+            get { return this.xpathaddress; }
+            set { this.xpathaddress = value; }
+        }
+    }
 }
--- onlineRegistration_app/App_Data/BusinessLayer/YearListBLL.cs
+++ onlineRegistration_app/App_Data/BusinessLayer/YearListBLL.cs
@@ -6,12 +6,26 @@ using System.Web.UI.WebControls;
 
 namespace onlineRegistration_app.BusinessLayer
 {
+    /// <summary>
+    /// Simple function to generate a list of years based on the current year.
+    /// </summary>
    public class YearListBLL
    {
+       /// <summary>
+       /// The padding for the year list.
+       /// Padding by 2 will start the list with the current year minus 2 and
+       /// end the list with the current year plus 2.
+       /// </summary>
       private int yearPadding = 2;
 
       public YearListBLL() { }
 
+       /// <summary>
+       /// Generates a List Item Collection of years starting with the current year minus yearPadding
+       /// and ending with the current year plus yearPadding.
+       /// For example, if the current year is 2015, the list will start with 2013 and end with 2017.
+       /// </summary>
+       /// <returns></returns>
       public ListItemCollection Select()
       {
          int curYear = DateTime.Now.Year;
